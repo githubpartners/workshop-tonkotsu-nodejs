271758179faaa5deebb123772f836210c13799c7
Pierluigi Cau <pierluigi@github.com>
2019-05-16 15:02:50 +0100

    Create server response with template

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
-const { fetchQuote, fetchZen } = require("./lib/utils");
-const https = require("https");
+const server = require("server");
+const chalk = require("chalk");
+const ora = require("ora");
 
-(async () => {
-  console.log("Fetching remote message... please wait.");
-  const msg = await fetchZen();
-  console.log(msg);
-})().catch(err => console.log("Oh noes!", err));
+const { error, get } = server.router;
+const { render, status } = server.reply;
+
+const { fetchZen } = require("./lib/utils");
+
+const log404 = ctx =>
+  console.log(chalk`{red 404}   {green ${ctx.method}}    {bold ${ctx.url}}`);
+
+const getZenQuote = async ctx => {
+  const spinner = ora("Fetching Zen message of the day from GitHub").start();
+  try {
+    ctx.zen = { msg: await fetchZen() };
+    // ctx.zen = { msg: await "test" }; // for debugging
+    spinner.succeed();
+  } catch (e) {
+    spinner.fail();
+    throw e;
+  }
+};
+
+server(
+  { log: "debug", env: "development" },
+  get("/", [
+    getZenQuote,
+    async ctx => {
+      return render("index.pug", { zen: ctx.zen });
+    }
+  ]),
+  // 404s
+  get(ctx => {
+    log404(ctx);
+    return status(404);
+  }),
+  error(ctx => status(500).send("Error: " + ctx.error.message))
+).then(ctx => {
+  console.log(chalk`
+  {green [ OK ]} Server launched on {bold http://localhost:${
+    ctx.options.port
+  }/}`);
+});
