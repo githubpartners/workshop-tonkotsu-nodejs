9f19df40d19661ff1ae885ab2c382f53865cc90a
Michael Lazenby <49193269+mikelazenby@users.noreply.github.com>
2020-05-29 11:28:48 +0100

    Update utils.test.js

--- test/utils.test.js
+++ test/utils.test.js
@@ -12,7 +12,7 @@ var mochaAsync = fn => {
 describe("Utils", function() {
   describe("greetings", function() {
     it("Says Hello World", function() {
-      assert.equal(utils.greetings("World"), "Hello Mike 👋👋");
+      assert.equal(utils.greetings("World"), "Hello Michael 👋👋");
     });
 
     it("Throws on missing params", function() {
