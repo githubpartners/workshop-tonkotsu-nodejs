fefbb7b88f8d0bcf6bed78be0d97857d5300e8e5
Aradhana Gupta <31731159+amrutsoftware@users.noreply.github.com>
2020-09-10 09:18:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const log404 = ctx =>
   console.log(chalk`{red 404}   {green ${ctx.method}}    {bold ${ctx.url}}`);
 
 const getZenQuote = async ctx => {
-  const spinner = ora1("Fetching Zen message of the day from GitHub").start();
+  const spinner = ora1"Fetching Zen message of the day from GitHub").start();
   try {
     ctx.zen = { msg: await fetchZen() };
     // ctx.zen = { msg: await "test" }; // for debugging
