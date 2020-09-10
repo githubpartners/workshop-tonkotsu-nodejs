9c7ae642616d2f24038ee9ee95019f010de9d47c
Aradhana Gupta <31731159+amrutsoftware@users.noreply.github.com>
2020-09-10 09:21:59 +0530

    test

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const server = require("server");
 const chalk = require("chalk");
-const ora = require("ora");
+//const ora = require("ora");
   
 const { error, get } = server.router;
 const { render, status } = server.reply;
 
-const { fetchZen } = require("./lib/utils")
+const { fetchZen } = require("./lib/utils");
 
 const log404 = ctx =>
   console.log(chalk`{red 404}   {green ${ctx.method}}    {bold ${ctx.url}}`);
